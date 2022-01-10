2fb6222449da8b9c61e6383a7bc4a4a680a0ab2b
Ankit Kumar Ghosh <76126678+heyankit78@users.noreply.github.com>
2022-01-10 13:45:09 +0530

    Create Column name from a given column number.cpp

---
+++ Amazon/Column name from a given column number.cpp
@@ -0,0 +1,14 @@
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // your code here
+        string temp = "";
+        while(n>0){
+            char ch = 'A'+(n-1)%26;
+            temp = ch + temp;
+            n = (n-1)/26;
+        }
+        return temp;
+    }
+};
