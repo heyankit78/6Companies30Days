71b0c80348fdd7e93680b8efe6ad0f8bc1b2af53
Ankit Kumar Ghosh <76126678+heyankit78@users.noreply.github.com>
2022-01-10 15:11:55 +0530

    Create IPL match.cpp

---
+++ Amazon/IPL match.cpp
@@ -0,0 +1,32 @@
+
+
+
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        // your code here
+        vector<int>ans;
+        int i = 0;
+        int j = 0;
+        list<int>l;
+        int mx = INT_MIN;
+        if(k>n){
+            ans.push_back(*max_element(arr.begin(),arr.end()));
+            return ans;
+        }
+        while(j<n){
+            while(!l.empty() && l.back() < arr[j]){
+                l.pop_back();
+            }
+            l.push_back(arr[j]);
+            if(j-i+1 < k)j++;
+            else if(j-i+1 == k){
+                ans.push_back(l.front());
+                if(l.front() == arr[i])l.pop_front();
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
